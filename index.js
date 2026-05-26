cdd2fc250b413e3d58858fa7e94acb8ec0305148
you@example.com <you@example.com>
2026-05-26 17:39:23 -0300

    aula01

---
+++ index.js
@@ -0,0 +1,93 @@
+1
+
+const nome = "Murillo";
+
+const idade = 16;
+
+const cidade = "Cascavel";
+
+
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
+
+
+2
+
+const a = 10;
+
+const b = 5;
+
+
+
+const soma = a + b;
+
+
+
+console.log(`A soma de ${a} + ${b} é ${soma}`);
+
+
+3
+
+const numero = 7;
+
+
+
+if (numero > 0) {
+
+  console.log("Número positivo");
+
+} else if (numero < 0) {
+
+  console.log("Número negativo");
+
+} else {
+
+  console.log("Número zero");
+
+}
+
+
+4
+
+const n1 = 7;
+
+const n2 = 8;
+
+const n3 = 6;
+
+
+
+const media = (n1 + n2 + n3) / 3;
+
+
+
+console.log(`A média é ${media}`);
+
+
+5
+
+const nota1 = 7;
+
+const nota2 = 5;
+
+const nota3 = 6;
+
+
+
+const mediaFinal = (nota1 + nota2 + nota3) / 3;
+
+
+
+if (mediaFinal >= 7) {
+
+  console.log(`Aprovado com média ${mediaFinal}`);
+
+} else if (mediaFinal >= 5) {
+
+  console.log(`Recuperação com média ${mediaFinal}`);
+
+} else {
+
+  console.log(`Reprovado com média ${mediaFinal}`);
+
+}
